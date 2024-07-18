72424492002021faee847733f2d2041385b853be
ocean0s <someone@somewhere.notanemail>
2024-07-18 17:53:30 +0200

    Improved present list

--- src/Components/ListPresentsComponent.js
+++ src/Components/ListPresentsComponent.js
@@ -59,18 +59,40 @@ let ListPresentsComponent = (props) => {
         <div className="item-list">
             <h2>My presents</h2>
             {message != "" && <p className="error">{message}</p> }
-            { presents.map( p => 
-                <div className="item">
-                    <h3>{p.name}</h3>
-                    <p>{p.description}</p>
-                    <p>{p.url}</p>
-                    <p>{p.price + " €"}</p>
-                    <p>{p.chosenBy == null ? "No one yet" : p.chosenBy }</p>
-                    <button className="action-button delete-present" onClick={() => deletePresent(p.id)}>Delete</button>
-                    <button className="action-button" onClick={() => editPresent(p.id)}>Edit</button>
-                </div>
-            )}
+            <table className="present-list">
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>URL</th>
+                    <th>Price</th>
+                    <th>Chosen by</th>
+                    <th>Actions</th>
+                </tr>
+                { presents.map( p=> 
+                    <tr>
+                        <td>{p.name}</td>
+                        <td>{p.description}</td>
+                        <td>{p.url}</td>
+                        <td>{p.price + " €"}</td>
+                        <td>{p.chosenBy == null ? "No one yet" : p.chosenBy}</td>
+                        <td>
+                            <button className="action-button delete-present" onClick={() => deletePresent(p.id)}>Delete</button>
+                            <button className="action-button" onClick={() => editPresent(p.id)}>Edit</button>
+                        </td>
+                    </tr>
+                )}
+            </table>
         </div> // TODO add better table, classes do not exist yet
+            // { presents.map( p => 
+            //     <div className="item">
+            //         <h3>{p.name}</h3>
+            //         <p>{p.description}</p>
+            //         <p>{p.url}</p>
+            //         <p>{p.price + " €"}</p>
+            //         <p>{p.chosenBy == null ? "No one yet" : p.chosenBy }</p>
+            //         <button className="action-button delete-present" onClick={() => deletePresent(p.id)}>Delete</button>
+            //         <button className="action-button" onClick={() => editPresent(p.id)}>Edit</button>
+            //     </div>
     )
 }
 
